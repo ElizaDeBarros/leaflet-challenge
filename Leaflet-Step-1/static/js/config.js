7fdee1bd3a3a1efdf2889e38a3400edd596621c9
Maria Eliza Medeiros Barros <eliza.medeiros@gmail.com>
2021-07-21 13:31:14 -0700

    final version

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZWxpemFkZWJhcnJvcyIsImEiOiJja3F5MXRvdWYxM3lzMnBvOHFwdzlnd2o0In0.B0Ke-8HifMeMcKmV1Bn0qw";
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,89 +1,128 @@
 // Store API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var link = "static/data/PB2002_boundaries.json";
+
+// Creates tectonic plates layerGroup
+var platesGroup = L.layerGroup();
 
 // Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
+d3.json(queryUrl).then(function(earthquakeData) {
+  d3.json(link).then(function(platesData) {
+    L.geoJSON(platesData, {
+      color: "orange"
+      }).addTo(platesGroup); 
+  });
 
-  // Creating map object
-  var myMap = L.map("map", {
-    center: [
-      38.69, -101.25],
-    zoom: 6,
+  
+  var locationEarthquake = [];
+  var earthquake = [];
+
+    // Setup logic earthquake color basese on its deptht 
+    for (var i = 0; i < earthquakeData.features.length; i++) {
+      var color = "";
+        if (earthquakeData.features[i].geometry.coordinates[2] >= -10 && earthquakeData.features[i].geometry.coordinates[2] <= 10) {
+          color = "#99ee00";
+        }
+        else if (earthquakeData.features[i].geometry.coordinates[2] > 10 && earthquakeData.features[i].geometry.coordinates[2] <= 30) {
+          color = "#d4ee00";
+        }
+        else if (earthquakeData.features[i].geometry.coordinates[2] > 30 && earthquakeData.features[i].geometry.coordinates[2] <= 50) {
+          color = "#ffe181";
+        }
+        else if (earthquakeData.features[i].geometry.coordinates[2] > 50 && earthquakeData.features[i].geometry.coordinates[2] <= 70) {
+          color = "#ffc000";
+        }
+        else if (earthquakeData.features[i].geometry.coordinates[2] > 70 && earthquakeData.features[i].geometry.coordinates[2] <= 90) {
+          color = "#edb9a9";
+        }
+        else {
+          color = "#fa735c";
+        };
+        
+        // pushes earthquake lat and long into a list
+        locationEarthquake.push([earthquakeData.features[i].geometry.coordinates[1], earthquakeData.features[i].geometry.coordinates[0]]);
+        
+      // Circles layer
+      earthquake.push(L.circleMarker(locationEarthquake[i], {
+          fillOpacity: 0.75,
+          color: "black",
+          stroke : true,
+          weight : 1,
+          fillColor: color,
+          // Adjust radius
+          radius:( earthquakeData.features[i].properties.mag)*2 
+      }).bindPopup("<h1> Place: " + earthquakeData.features[i].properties.place + "</h1> <hr> <h3>Time: " + earthquakeData.features[i].properties.time + "</h3>"));
+    };
+    // Creates earthquake layerGroup
+    var earthquakeGroup = L.layerGroup(earthquake);
+
+    // Define variables for tile layers
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "satellite-v9",
+      accessToken: API_KEY
     });
 
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
-  }).addTo(myMap);
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "outdoors-v11",
+      accessToken: API_KEY
+    });
+
+    var grey = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY
+    });
+
+    // Only one base layer can be shown at a time
+    var baseMaps = {
+      Satellite: satellite,
+      Greyscalle: grey,
+      Outdoors: outdoors
+    };
+
+    // Overlays that may be toggled on or off
+    var overlayMaps = {
+      "Earthquakes": earthquakeGroup,
+      "Tectonic Plates": platesGroup
+    };
+
+    // Create map object and set default layers
+    var myMap = L.map("map", {
+      center: [38.69, -101.25],
+      zoom: 4,
+      layers: [satellite, earthquakeGroup, platesGroup]
+    });
 
 
-  console.log(data.features);
-  var location = [];
-  for (var i = 0; i < data.features.length; i++) {
-    
-    var color = "";
-      if (data.features[i].geometry.coordinates[2] >= -10 && data.features[i].geometry.coordinates[2] <= 10) {
-        color = "#99ee00";
-      }
-      else if (data.features[i].geometry.coordinates[2] > 10 && data.features[i].geometry.coordinates[2] <= 30) {
-        color = "#d4ee00";
-      }
-      else if (data.features[i].geometry.coordinates[2] > 30 && data.features[i].geometry.coordinates[2] <= 50) {
-        color = "#ffe181";
-      }
-      else if (data.features[i].geometry.coordinates[2] > 50 && data.features[i].geometry.coordinates[2] <= 70) {
-        color = "#ffc000";
-      }
-      else if (data.features[i].geometry.coordinates[2] > 70 && data.features[i].geometry.coordinates[2] <= 90) {
-        color = "#edb9a9";
-      }
-      else {
-        color = "#fa735c";
-      };
-      
-      
-      location.push([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]]);
+    // Pass map layers into the layer control
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
-    // Add circles to map
-    L.circleMarker(location[i], {
-        fillOpacity: 0.75,
-        color: "black",
-        stroke : true,
-        weight : 1,
-        fillColor: color,
-        // Adjust radius
-        radius:( data.features[i].properties.mag)*2 
-    }).bindPopup("<h1> Place: " + data.features[i].properties.place + "</h1> <hr> <h3>Time: " + data.features[i].properties.time + "</h3>").addTo(myMap);
-  
-  };
-
-
+    // Setup legend
     function getColor(d) {
       return d ==='-10-10' ? '#99ee00' :
-            d ==='10-30' ? '#d4ee00' :
-            d ==='30-50' ? '#ffe181' :
-            d ==='50-70' ? '#ffc000' :
-            d ==='70-90' ? '#edb9a9' : '#fa735c';
+              d ==='10-30' ? '#d4ee00' :
+              d ==='30-50' ? '#ffe181' :
+              d ==='50-70' ? '#ffc000' :
+              d ==='70-90' ? '#edb9a9' : '#fa735c';
     };
 
 
     var legend = L.control({position: 'bottomright'});
     legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend');
+      categories = ['-10-10','10-30','30-50','50-70','70-90','90+'];
 
-        var div = L.DomUtil.create('div', 'info legend');
-        categories = ['-10-10','10-30','30-50','50-70','70-90','90+'];
-
-        for (var i = 0; i < categories.length; i++) {
-
-                div.innerHTML += 
-                  '<i style="background:' + getColor(categories[i]) + '"></i> ' +
-        (categories[i] ? categories[i] : '90+')+'<br>';
-       
-
-        };
+      for (var i = 0; i < categories.length; i++) {
+        div.innerHTML += 
+          '<i style="background:' + getColor(categories[i]) + '"></i> ' +
+          (categories[i] ? categories[i] : '90+')+'<br>';
+      };
       return div;
     };
     legend.addTo(myMap);
